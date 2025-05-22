fb7e07a7dcaa9828b06447a1ea25d94a32719c0e
akshat-ttn <akshat.srivastava@tothenew.com>
2025-05-22 15:24:07 +0530

    Added User Entity

---
+++ src/main/java/com/apnabaazar/userprofile/model/User.java
@@ -0,0 +1,50 @@
+package com.apnabaazar.userprofile.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@SQLDelete(sql = "update users set is_deleted=true where id=?")
+@SQLRestriction(value = "is_deleted = false")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private List<Hobby> hobbies = new ArrayList<>();
+
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String password;
+    private boolean isDeleted = false;
+
+    private boolean isActive = false;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
